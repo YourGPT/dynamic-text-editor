9615a82bbe8d863ad2ad1d5c181b39fcaba68012
Sahil <sk950121@gmail.com>
2025-03-10 13:50:29 +0530

    build fix

--- test/vite.config.ts
+++ test/vite.config.ts
@@ -4,7 +4,19 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: "externalize-styled-deps",
+      resolveId(id) {
+        // Externalize all styled-components and emotion related packages
+        if (id.includes("styled-components") || id.includes("@emotion/")) {
+          return { id, external: true };
+        }
+        return null;
+      },
+    },
+  ],
   resolve: {
     preserveSymlinks: true,
     alias: {
@@ -16,16 +28,23 @@ export default defineConfig({
       include: [/dynamic-text-editor/, /node_modules/],
     },
     rollupOptions: {
-      external: ["tslib", "@emotion/is-prop-valid"],
+      external: ["tslib", "styled-components", "@emotion/is-prop-valid", "@emotion/sheet", "@emotion/stylis", "@emotion/unitless", "@emotion/react", "hoist-non-react-statics", "shallowequal"],
       output: {
         globals: {
           tslib: "tslib",
+          "styled-components": "styled",
           "@emotion/is-prop-valid": "isPropValid",
+          "@emotion/sheet": "emotionSheet",
+          "@emotion/stylis": "stylis",
+          "@emotion/unitless": "unitless",
+          "@emotion/react": "emotionReact",
+          "hoist-non-react-statics": "hoistNonReactStatics",
+          shallowequal: "shallowequal",
         },
       },
     },
   },
   optimizeDeps: {
-    include: ["tslib"],
+    include: ["tslib", "styled-components"],
   },
 });
